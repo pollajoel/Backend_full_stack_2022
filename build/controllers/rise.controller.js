67995c6e1a8afe51ee4bfd5b2c2d4b87e6a3fae3
pollajoel <pollajoel2017@gmail.com>
2021-11-11 17:49:30 +0100

    update

--- build/controllers/rise.controller.js
+++ build/controllers/rise.controller.js
@@ -5,11 +5,10 @@ const Op = db.Sequelize.Op;
 
 
 exports.findAll = async(req, res)=>{
-
-  rise.findAll({}).then(data => {
-    res.send(data).status(200);
-  }).catch(err=>{
-    res.status(500).send({message: err.message || "an error occur"});
-  });
+    rise.findAll({}).then(data => {
+      res.send(data).status(200);
+    }).catch(err=>{
+      res.status(500).send({message: err.message || "an error occur"});
+    });
   
 }
