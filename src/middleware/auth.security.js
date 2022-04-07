7dd6f4fd7003ccb5446a60fc49f10479fe3e4553
pollajoel <pollajoel2017@gmail.com>
2022-04-07 14:51:28 +0200

    update

--- src/middleware/auth.security.js
+++ src/middleware/auth.security.js
@@ -10,7 +10,7 @@ function getUserId(req, authToken) {
   if (req) {
     const authHeader = req.headers.authorization;
     if (authHeader) {
-      const token = authHeader.replace('Bearer ', '');
+      const token = authHeader.replace('JWT ', '');
       if (!token) {
         throw new Error('No token found');
       }
